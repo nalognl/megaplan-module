bdbb2b037c0786464685134eb736ebdb0a1c2a0d
SerhiiCho <serhiicho@protonmail.com>
2020-01-16 19:12:27 +0200

    Removed methods from TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,25 +3,8 @@
 namespace Nalognl\MegaplanModule\Tests;
 
 use PHPUnit\Framework\TestCase as PHPUnit;
-use ReflectionClass;
 
 class TestCase extends PHPUnit
 {
-    public function setUp(): void
-    {
-        parent::setUp();
-    }
-
-    public function tearDown(): void
-    {
-        parent::tearDown();
-    }
-
-    protected function execMethod(string $class, string $method, object $object, $method_args = [])
-    {
-        $reflection = new ReflectionClass($class);
-        $new_method = $reflection->getMethod($method);
-        $new_method->setAccessible(true);
-        return $new_method->invoke($object, ...$method_args);
-    }
+    //
 }
